aaf9ce520c4b0e8a42a10c6a2578dccc682f9fc0
ibrahimacar50-stack <ibrahimacar50@gmail.com>
2025-12-27 03:32:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,24 @@
-const CACHE_NAME = "credit-pwa-v1";
+const CACHE_NAME = "pwa-credit-v1";
+
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
